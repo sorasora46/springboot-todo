31d34631d1799f91bd947eb88a1b50b240fab30f
sorasora46 <sorrawit02546@gmail.com>
2023-12-03 03:52:06 +0700

    Setup DTA

---
+++ src/main/java/com/sora/todo/services/TodoService.java
@@ -0,0 +1,53 @@
+package com.sora.todo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.sora.todo.models.Todo;
+import com.sora.todo.repositories.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Optional<Todo> getTodoById(Integer id) {
+        return todoRepository.findById(id);
+    }
+
+    public Integer createTodo(String todo) {
+        var newTodo = new Todo(todo);
+        Todo result = todoRepository.save(newTodo);
+
+        return result.getId();
+    }
+
+    public Boolean updateTodo(Integer id) {
+        Boolean isTodoExist = todoRepository.existsById(id);
+
+        if (!isTodoExist)
+            return false;
+
+        return true;
+    }
+
+    public Boolean deleteTodo(Integer id) {
+        Boolean isTodoExist = todoRepository.existsById(id);
+
+        if (!isTodoExist)
+            return false;
+
+        todoRepository.deleteById(id);
+        Boolean isTodoDeleted = todoRepository.existsById(id);
+
+        if (isTodoDeleted)
+            return false;
+
+        return true;
+    }
+}
